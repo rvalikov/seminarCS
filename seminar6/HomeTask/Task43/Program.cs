69de65a2cfc8930f09fb4b622352f5c6ae817212
rvalikov <r.valikov@mail.ru>
2022-12-11 12:44:42 +0300

    HomeTask Seminar6

---
+++ seminar6/HomeTask/Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.WriteLine("Введите коэффициенты");
+Console.Write("K1= ");
+decimal k1=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine();
+Console.Write("B1= ");
+decimal b1=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine();
+Console.Write("K2= ");
+decimal k2=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine();
+Console.Write("B2= ");
+decimal b2=Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Ваши уравнения:");
+Console.WriteLine($"y= {k1} x + {b1}");
+Console.WriteLine($"y= {k2} x + {b2}");
+decimal x=(b2-b1)/(k1-k2);
+decimal y=k1*x+b1;
+Console.WriteLine($"Точка пересечения ( {x}  ,  {y})");
+
+
+
+
+
---
+++ seminar6/HomeTask/task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+
+Console.WriteLine("Ввежите количество чисел");
+int size= Convert.ToInt32(Console.ReadLine());
+int [] array = new int[size];
+int count=0;
+for(int i=0;i<size;i++){
+    Console.WriteLine("Введите  число");
+
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0){count++;}
+    
+}
+Console.WriteLine($"Вы ввели: {String.Join(", ",array)}");
+
+Console.WriteLine($"Количество положительных: {count}");
